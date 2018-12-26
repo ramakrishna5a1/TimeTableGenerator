ea6352e32cb6bac28fd22c03c1e204f11fe207ef
ramakrishna5a1 <saiduramakrishna25@gmail.com>
2018-12-26 16:28:37 +0530

    firebase method updated

--- app/src/main/java/rk/com/users/AdminActivity.java
+++ app/src/main/java/rk/com/users/AdminActivity.java
@@ -1,17 +1,28 @@
 package rk.com.users;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class AdminActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AdminActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
     }
 
-    public void registerUser(View view) {
+    public void registerUser(View view)
+    {
+        new FireBase().userData();
     }
 }
+
--- app/src/main/java/rk/com/users/FireBase.java
+++ app/src/main/java/rk/com/users/FireBase.java
@@ -3,11 +3,18 @@ package rk.com.users;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-class FireBase {
+class FireBase
+{
     DatabaseReference mDatabase;
+    String users[] = {"admin", "faculty", "hod", "student"};
 
-    public void userData() {
+    public FireBase()
+    {
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.setValue("krishna");
+    }
+
+    public void userData()
+    {
+        mDatabase.child(users[1]).child("sir2").setValue("2");
     }
 }
